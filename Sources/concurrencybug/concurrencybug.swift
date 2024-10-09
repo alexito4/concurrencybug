d343f18a0ba0f54d6693323de93081745287b42c
Alejandro Martínez <alexito4@gmail.com>
2024-10-09 11:55:10 +0200

    Clarified is not a bug and link to the forums

--- Sources/concurrencybug/concurrencybug.swift
+++ Sources/concurrencybug/concurrencybug.swift
@@ -1,12 +1,33 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-@preconcurrency import old
+//@preconcurrency import old
 
 //class File {
 //    func read() -> String { "" }
 //}
 
+struct File {
+    let ns = NonSendable()
+    
+    func read() -> String { "" }
+    
+    class NonSendable {}
+}
+
+func inSameFunction() async {
+    let files: [File] = []
+    await withTaskGroup(of: Void.self) { group in
+        for file in files {
+            group.addTask {
+                let c = file.read()
+                print(c)
+            }
+        }
+    }
+}
+
+// The compiler is right to raise an error, see https://forums.swift.org/t/region-isolation-of-array-elements/75231/2
 func someFunction(
     files: [File]
 ) async throws {
